69ce8d2db5464b42884268129afbe58871ec5bc9
Shadi-Shwiyat <raz611103@gmail.com>
2024-03-27 16:02:10 -0500

    created MySQL db script to set up db according to uml doc

---
+++ db_setup.sql
@@ -0,0 +1,44 @@
+-- MySQL script that creates a new database
+-- and structures it to hold information
+-- reguarding 'The Joy of Painting' TV show
+
+CREATE DATABASE IF NOT EXISTS the_joy_of_painting;
+USE the_joy_of_painting;
+
+CREATE TABLE `episodes` (
+  `painting_index` integer,
+  `title` text,
+  `date` date,
+  `season` integer,
+  `episode` integer,
+  `notes` text,
+  `img_src` text,
+  `youtube_src` text,
+  PRIMARY KEY (`painting_index`)
+);
+
+CREATE TABLE `episodes_colors` (
+  `painting_index` integer,
+  `colors_id` integer,
+  `num_colors` integer,
+  PRIMARY KEY (`painting_index`, `colors_id`)
+);
+
+CREATE TABLE `episode_subjects` (
+  `painting_index` integer,
+  `subject_id` integer,
+  PRIMARY KEY (`painting_index`, `subject_id`)
+);
+
+CREATE TABLE `colors` (
+  `colors_id` integer,
+  `color_name` text,
+  `hex_value` text,
+  PRIMARY KEY (`colors_id`)
+);
+
+CREATE TABLE `subjects` (
+  `subject_id` integer,
+  `subject_name` text,
+  PRIMARY KEY (`subject_id`)
+);
